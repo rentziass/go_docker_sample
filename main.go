7470f046be3cb2da5fb6821aaed34d1e178855b5
Francesco Renzi <rentziass@gmail.com>
2018-01-23 17:21:52 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Altrimenti gabri non ci crede")
+	fmt.Println("Starting off on port 3000")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":3000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello from a container!")
+}
